336f43c36826f46a072b8b70bb22e523c9143074
Jeong Wook Oh <oh.jeongwook@gmail.com>
2015-02-28 00:04:03 -0600

    Added ID pane to display id and it's count

--- CanBusHacker.py
+++ CanBusHacker.py
@@ -95,12 +95,146 @@ class PacketTable(QAbstractTableModel):
 		self.endInsertRows()
 		self.dataChanged.emit(insert_row,insert_row)
 	
+class TreeItem(object):
+	def __init__(self,data,parent=None):
+		self.parentItem=parent
+		self.itemData=data
+		self.childItems=[]
+		
+	def appendChild(self,item):
+		self.childItems.append(item)
+		
+	def child(self,row):
+		return self.childItems[row]
+		
+	def childCount(self):
+		return len(self.childItems)
+		
+	def children(self):
+		return self.childItems
+		
+	def columnCount(self):
+		return len(self.itemData)
+	
+	def data(self,column):
+		try:
+			return self.itemData[column]
+		except:
+			import traceback
+			traceback.print_exc()
+			
+	def parent(self):
+		return self.parentItem
+		
+	def row(self):
+		if self.parentItem:
+			return parentItem.childItems.index(self)
+			
+		return 0
+		
+class TreeModel(QAbstractItemModel):
+	def __init__(self,root_item,parent=None):
+		super(TreeModel,self).__init__(parent)
+		self.rootItem=TreeItem(root_item)
+		self.ID2Item={}
+		
+	def columnCount(self,parent):
+		if parent.isValid():
+			return parent.internalPointer().columnCount()
+		else:
+			return self.rootItem.columnCount()
+		
+	def rowCount(self,parent):
+		if parent.column()>0:
+			return 0
+			
+		if not parent.isValid():
+			parentItem=self.rootItem
+		else:
+			parentItem=parent.internalPointer()
+			
+		return parentItem.childCount()
+		
+	def data(self,index,role):
+		if not index.isValid():
+			return None
+			
+		if role==Qt.DisplayRole:
+			item=index.internalPointer()
+			return item.data(index.column())
+			
+		return None
+		
+	def flags(self,index):
+		if not index.isValid():
+			return Qt.NoItemFlags
+			
+		return Qt.ItemIsEnabled|Qt.ItemIsSelectable
+	
+	def index(self,row,column,parent):
+		if not self.hasIndex(row,column,parent):
+			return QModelIndex()
+			
+		if not parent.isValid():
+			parentItem=self.rootItem
+		else:
+			parentItem=parent.internalPointer()
+			
+		childItem=parentItem.child(row)
+		if childItem:
+			return self.createIndex(row,column,childItem)
+		else:
+			return QModelIndex()
+			
+	def parent(self,index):
+		if not index.isValid():
+			return QModelindex()
+			
+		childItem=index.internalPointer()
+		parentItem=childItem.parent()
+		
+		if parentItem is not None:
+			if parentItem==self.rootItem:
+					return QModelIndex()
+					
+			return self.createIndex(parentItem.row(),0,parentItem)
+		return QModelIndex()
+		
+	def headerData(self,section,orientation,role):
+		if orientation==Qt.Horizontal and role==Qt.DisplayRole:
+			return self.rootItem.data(section)
+			
+		return None
+		
+	def addIDData(self,id,count):
+		insert_row=0
+		self.beginInsertRows(QModelIndex(), insert_row, insert_row)
+		
+		if self.ID2Item.has_key(id):
+			tree_item=self.ID2Item[id]
+			tree_item.itemData[1]="%d" % count
+		else:
+			tree_item=TreeItem([str(id),"%d" % count])
+			self.ID2Item[id]=tree_item
+			self.rootItem.appendChild(tree_item)
+			
+		self.endInsertRows()
+		self.dataChanged.emit(insert_row,insert_row)
+	
 class MainWindow(QMainWindow):
 	DebugPacketLoad=0
 	def __init__(self,com='',log_db=''):
 		super(MainWindow,self).__init__()
 		self.setWindowTitle("CanBusHacker")
 		
+		vertical_splitter=QSplitter()
+		vertical_splitter.setOrientation(Qt.Vertical)
+		horizontal_splitter=QSplitter()
+		self.idTreeView=QTreeView()
+		self.idTreeModel=TreeModel(("ID","Count"))
+		self.idTreeView.setModel(self.idTreeModel)
+		horizontal_splitter.addWidget(self.idTreeView)
+			
 		self.PacketTableView=QTableView()
 		vheader=QHeaderView(Qt.Orientation.Vertical)
 		#vheader.setResizeMode(QHeaderView.ResizeToContents)
@@ -110,7 +244,16 @@ class MainWindow(QMainWindow):
 		self.PacketTableView.setSelectionBehavior(QAbstractItemView.SelectRows)		
 		self.PacketTableModel=PacketTable(self)
 		self.PacketTableView.setModel(self.PacketTableModel)
-		
+
+		horizontal_splitter.addWidget(self.PacketTableView)		
+		horizontal_splitter.setStretchFactor(0,0)
+		horizontal_splitter.setStretchFactor(1,1)
+
+		vertical_splitter.addWidget(horizontal_splitter)
+		self.logWidget=QTextEdit()
+		vertical_splitter.addWidget(self.logWidget)
+		vertical_splitter.setStretchFactor(0,1)
+		vertical_splitter.setStretchFactor(1,0)		
 		
 		if self.DebugPacketLoad>0:
 			Conn = sqlite3.connect(log_db)
@@ -122,7 +265,7 @@ class MainWindow(QMainWindow):
 			
 		main_widget=QWidget()
 		vlayout=QVBoxLayout()
-		vlayout.addWidget(self.PacketTableView)
+		vlayout.addWidget(vertical_splitter)
 		main_widget.setLayout(vlayout)
 		self.setCentralWidget(main_widget)
 		self.show()
@@ -139,6 +282,7 @@ class MainWindow(QMainWindow):
 		self.IDCountMap={}
 		self.LastPayloadMap={}
 		self.TimeMap={}
+
 	def getCanMessage(self,(current_time,id,bytes)):
 		self.PacketTableModel.addPacket((current_time,id,bytes))
 		self.PacketTableView.scrollToBottom()
@@ -154,6 +298,8 @@ class MainWindow(QMainWindow):
 		else:
 			elapsed_time=0
 		self.TimeMap[id]=current_time
+		
+		self.idTreeModel.addIDData(id,self.IDCountMap[id])
 		
 if __name__=='__main__':
 	import sys
